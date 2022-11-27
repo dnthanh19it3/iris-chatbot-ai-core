5edd501d61d40eb4cc10e29d72508ab32f1dd3fa
Thanh Do <thanhdn@irtech.com.vn>
2022-11-27 16:11:03 +0700

    Init

---
+++ ultils/getjson.py
@@ -0,0 +1,9 @@
+import urllib.request, json
+def getJson():
+    # with urllib.request.urlopen("http://localhost/zalo-chatbot/export") as url:
+    #     data = json.loads(url.read().decode())
+    #     return data
+
+    with open('../static_intent/intents.json', 'r', encoding='utf8') as json_data:
+        data = json.load(json_data)
+        return data
