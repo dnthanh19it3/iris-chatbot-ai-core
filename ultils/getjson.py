f02aa862b5ea404a2032ea286c76a859e58cf89e
Thanh Do <thanhdn@irtech.com.vn>
2023-01-13 03:58:46 +0700

    prod mode on

--- ultils/getjson.py
+++ ultils/getjson.py
@@ -1,6 +1,7 @@
 import urllib.request, json
 def getJson(project_id):
-    with urllib.request.urlopen("http://localhost/console/get-dataset/" + project_id) as url:
+    with urllib.request.urlopen("https://irisbot-ai.iristech.live//console/get-dataset/" + project_id) as url:
+    # with urllib.request.urlopen("http://localhost/console/get-dataset/" + project_id) as url:
         data = json.loads(url.read().decode())
         print("Got intent")
         with open('static_intent/' + project_id + '.json', 'w',  encoding='utf8') as f:
